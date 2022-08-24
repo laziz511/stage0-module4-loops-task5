d534ae55a3c83842463d941ff5aa84166d976b00
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-24 09:27:58 +0500

    initial commit

--- src/test/java/school/mjc/stage0/loops/task5/TriangleTest.java
+++ src/test/java/school/mjc/stage0/loops/task5/TriangleTest.java
@@ -3,59 +3,31 @@ package school.mjc.stage0.loops.task5;
 import school.mjc.stage0.base.BaseIOTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class TriangleTest extends BaseIOTest {
 
     @Test
     void printTriangle() {
-        String expected = """
-                8   \s
-                88  \s
-                888 \s
-                8888\s
-                88888
-                """;
-
-        String expected2 = """
-                8
-                88
-                888
-                8888
-                88888
-                """;
-
+        String expected = "8    \n" +
+                "88   \n" +
+                "888  \n" +
+                "8888 \n" +
+                "88888\n";
         Triangle triangle = new Triangle();
 
         triangle.printTriangle(5);
 
-        String checked = updateLineSpliterators(outContent.toString());
-
-        assertTrue(expected.equals(checked) || expected2.equals(checked));
-
+        assertOutEquals(expected);
     }
 
     @Test
     void printTrianglePrintsMin() {
-        String expected = """
-                8 \s
-                88\s
-                888
-                """;
-
-        String expected2 = """
-                8
-                88
-                888
-                """;
-
+        String expected = "8  \n" +
+                "88 \n" +
+                "888\n";
         Triangle triangle = new Triangle();
 
         triangle.printTriangle(3);
 
-        String checked = updateLineSpliterators(outContent.toString());
-
-        assertTrue(expected.equals(checked) || expected2.equals(checked));
-
+        assertOutEquals(expected);
     }
 }
